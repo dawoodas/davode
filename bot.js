4dd4ed4f629c58bfc65a917a7d8b0050e15afead
dawoodas <42155503+dawoodas@users.noreply.github.com>
2019-05-29 05:44:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- 
+const prefix = '$'
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
- 
- 
- 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : Diamond Codes`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers ' #Riyderz Games|$help' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(argresult, "https://www.twitch.tv/idk");//حقوق دايموند كودز
-client.user.setStatus("online")
- 
-});
+    console.log(`~~~~~~~~~~~~~~~~~`);
+    console.log(`Logging into Discord`);
+    console.log(`~~~~~~~~~~~~~~~~~~~~~`);
+    console.log(`on  ${client.guilds.size} Servers `);
+    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~`);
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setGame(`#Riyderz GAMES|$Help`,"http://twitch.tv/y04zgamer")
+    client.user.setStatus("dnd")
+ });
  
  
  client.on('message', message => {
