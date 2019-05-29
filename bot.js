154e5a20396c6fce56045fccb5ff4710f2bf54a3
dawoodas <42155503+dawoodas@users.noreply.github.com>
2019-05-29 05:47:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
     console.log(`on  ${client.guilds.size} Servers `);
     console.log(`~~~~~~~~~~~~~~~~~~~~~~~~`);
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setGame(`#Riyderz GAMES|$Help`,"http://twitch.tv/y04zgamer")
+    client.user.setGame(`#Riyderz Games |$Help`,"http://twitch.tv/y04zgamer")
     client.user.setStatus("dnd")
  });
  
